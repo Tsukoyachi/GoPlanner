184bc1bcc6f362a966f0484b7c66ea6dabf5250c
Tsukoyachi <Axel.delille31@gmail.com>
2024-06-14 15:42:57 +0700

    feat: Addition of swagger documentation and endpoint

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	httpSwagger "github.com/swaggo/http-swagger"
 
 	"github.com/tsukoyachi/goplanner/handlers"
 )
@@ -17,6 +18,19 @@ func main() {
 	router.HandleFunc("/todo", handlers.AddTask).Methods("POST")
 	router.HandleFunc("/todo/{id}", handlers.UpdateTask).Methods("PUT")
 	router.HandleFunc("/todo/{id}", handlers.DeleteTask).Methods("DELETE")
+
+	// Serve Swagger UI files
+	//router.PathPrefix("/docs/").Handler(http.StripPrefix("/docs/", http.FileServer(http.Dir("./swagger-ui/"))))
+
+	// Serve Swagger UI
+	router.PathPrefix("/docs/").Handler(httpSwagger.Handler(
+		httpSwagger.URL("http://localhost:8080/swagger"), // URL to your Swagger JSON doc
+	))
+
+	// Serve the service.swagger.yaml file
+	router.HandleFunc("/swagger", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./docs/v1/service.swagger.yml")
+	})
 
 	router.HandleFunc("/version", handlers.VersionHandler).Methods("GET")
 	router.HandleFunc("/", handlers.GreetingHandler).Methods("GET")
